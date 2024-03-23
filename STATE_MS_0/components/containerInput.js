d5b016e6d41332dd7b790f55c5672080f0a89377
oucheoof <143434428+oucheoof@users.noreply.github.com>
2024-03-23 14:25:20 +0100

    Fixade lägga till spel men det gör det inte via state :(

--- STATE_MS_0/components/containerInput.js
+++ STATE_MS_0/components/containerInput.js
@@ -1,14 +1,51 @@
 function rendercontainerInput(parentID){
-    let inputTextDOM = document.createElement("input")
-    parentDOM = document.getElementById(parentID);
+
+    parentDOM = document.getElementById(parentID); // "Wrapper", state.js
+
+
+    let inputTextDOM = document.createElement("input");
     inputTextDOM.type =  "text";
     parentDOM.appendChild(inputTextDOM);
 
-    //Fortsätt skapa input
-
-    let inputNumberDOM = document.createElement("input")
-    parentDOM = document.getElementById(parentID);
+    let inputNumberDOM = document.createElement("input");
     inputNumberDOM.type =  "number";
     console.log("here");
     parentDOM.appendChild(inputNumberDOM);
+
+    let buttonDOM = document.createElement("button");
+    buttonDOM.id = "button_Games";
+    buttonDOM.textContent = "Game";
+    parentDOM.appendChild(buttonDOM);
+
+    let button2DOM = document.createElement("button");
+    button2DOM.id = "button_Character";
+    button2DOM.textContent = "Character";
+    parentDOM.appendChild(button2DOM);
+
+
+
+    buttonDOM.addEventListener("click", function(){
+        let textValue = inputTextDOM.value;
+        let numberValue = inputNumberDOM.value;
+
+        let containerOneTarget = document.getElementById("containerOne")
+
+        console.log(textValue);
+        console.log(numberValue);
+
+        addNewRow(textValue, numberValue, containerOneTarget);
+    })
+
+    button2DOM.addEventListener("click", function(){
+        let textValue = inputTextDOM.value;
+        let numberValue = inputNumberDOM.value;
+
+        let containerTwoTarget = document.getElementById("containerTwo")
+
+        console.log(textValue);
+        console.log(numberValue);
+
+        addNewRow(textValue, numberValue, containerTwoTarget);
+    })
+
 }
---
+++ STATE_MS_0/logic/newRow.js
@@ -0,0 +1,10 @@
+function addNewRow(textValue, numberValue, targetContainerID){
+    let newRow = document.createElement("div");
+
+    newRow.textContent = `Text: ${textValue}, Rating: ${numberValue}`;
+
+    targetContainerID.appendChild(newRow);
+    console.log("here");
+
+    rendercontainerInput();
+}
