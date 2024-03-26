aedf1409f1545393e7e7dd2bc1e4825d3c96e1a2
oucheoof <143434428+oucheoof@users.noreply.github.com>
2024-03-26 16:40:59 +0100

    Update containerInput.js

--- STATE_MS_0/components/containerInput.js
+++ STATE_MS_0/components/containerInput.js
@@ -30,10 +30,10 @@ function rendercontainerInput(parentID){
         let numberValue = inputNumberDOM.value;
 
         STATE.entityOne.push({
-            message:`Text: ${textValue}`,
-            rank: `Number: ${textNumber}`,
-            
-             
+            /* message: `idValue: ${idValue}, Text: ${textValue}, Number: ${numberValue}`, */ 
+            id: idValue,
+            title: textValue,
+            rank: numberValue
         });
         
         idValue++;
