c52a0cf2990b46fb46dfee56f739ae65aa99ed9c
oucheoof <143434428+oucheoof@users.noreply.github.com>
2024-03-27 23:38:16 +0100

    Update deleteButton.js

--- STATE_MS_0/components/deleteButton.js
+++ STATE_MS_0/components/deleteButton.js
@@ -8,21 +8,11 @@ function renderDeleteButton(parentID){
     for (let i = 0; i < entityOne.length; i++) {
 
         deleteButtonDOM.id = `deleteButton_${i}`; // Assign a unique ID based on index i
-    }
+    } //Fixa id
 
     parentWrapper.appendChild(deleteButtonDOM);
 
     deleteButtonDOM.addEventListener("click", function(){
-        for (let i = 0; i < entityOne.length; i++) {
-            let entity = entityOne[i];
-
-            if(entity.id == deleteButtonDOM.id){
-                entityOne.splice(i, 1); // Remove the object at index i
-                console.log("true");
-
-                break; // Stop the loop after removing the object
-
-            }
-        }
-    })    
+        //Fixa delete
+    })
 }
