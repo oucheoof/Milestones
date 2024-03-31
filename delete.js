1669e985215bf9ccb7bb9d083672fd771e67dedc
oucheoof <143434428+oucheoof@users.noreply.github.com>
2024-03-31 19:34:19 +0200

    f

---
+++ delete.js
@@ -0,0 +1,21 @@
+function renderDelete(){
+    deleteDOM = document.createElement("button");
+    deleteDOM.textContent = "DELETE"
+    deleteDOM.id = "deleteButton";
+
+    mainDiv = document.getElementById("main");
+    parentDOM = mainDiv;
+
+    parentDOM.appendChild(deleteDOM);
+
+
+    console.log(deleteDOM);
+
+    function deleteLogic()
+
+}
+
+function deleteLogic(button, parent){
+    addEventListener("click")
+
+}
