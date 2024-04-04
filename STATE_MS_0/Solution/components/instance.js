168c2bcad842b8e569a7cfa314ebbccd33693199
Stephen Doan <stephendoan@hotmail.com>
2024-04-04 16:20:01 +0200

    Rensat bort lösning

--- STATE_MS_0/Solution/components/instance.js
+++
@@ -1,7 +0,0 @@
-function renderInstance( parentID, instanceData) {
-
-    const DOM = document.createElement( 'p');
-    DOM.textContent = instanceData.message;
-    document.getElementById( parentID).append(DOM);
-
-}
